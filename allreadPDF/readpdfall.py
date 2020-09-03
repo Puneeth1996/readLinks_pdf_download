c402f8e2bb678d7a58e61c89c9703d1dd5e19848
Puneeth P <puneeth1996p@gmail.com>
2020-09-03 18:19:14 +0530

    quick comit with code

--- allreadPDF/readpdfall.py
+++ allreadPDF/readpdfall.py
@@ -1,20 +1,27 @@
 from glob import glob
 import os, shutil
-import pikepdf
+import pikepdf, pdfplumber
 
 PATH = r'C:\Users\punee\Desktop\readLinks_pdf_download\test'
 EXT = "*.pdf"
 temp_cfn = [file
                  for path, subdir, files in os.walk(PATH)
                  for file in glob(os.path.join(path, EXT))]
-urls = []
+
+
 for file in temp_cfn:
+    
+    pdf = pdfplumber.open(file)
+    page = pdf.pages[0]
+    text = page.extract_text()
+    print( "\n\n", text )
+    pdf.close()
+    
+    
     pdf_file = pikepdf.Pdf.open(file)
+    
     for page  in pdf_file.pages:
         for annots in page.get('/Annots'):
             uri = annots.get('/A').get('/URI')
             if(uri is not None):
-                print( "\n\n", '[FILE] '+ str(file) +'\n [+] URL FOUND ' , uri, "\n\n" )
-                urls.append(urls)
-
-print("[*] Total URLs extracted:", len(urls))
+                print(  '[FILE] '+ str(file) +'\n [+] URL FOUND ' , uri, "\n\n" )
