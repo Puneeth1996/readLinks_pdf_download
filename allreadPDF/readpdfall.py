e814c12b0818c3c9bc599e136426cb6ce2d0e073
Puneeth P <puneeth1996p@gmail.com>
2020-09-03 18:08:25 +0530

    quick commit wiht code

--- allreadPDF/readpdfall.py
+++ allreadPDF/readpdfall.py
@@ -1,13 +1,5 @@
 from glob import glob
 import os, shutil
-import csv
-import sys, re
-import time
-import collections
-import dateutil.parser
-from datetime import datetime
-import dateparser
-import pandas as pd
 import pikepdf
 
 PATH = r'C:\Users\punee\Desktop\readLinks_pdf_download\test'
@@ -15,7 +7,6 @@ EXT = "*.pdf"
 temp_cfn = [file
                  for path, subdir, files in os.walk(PATH)
                  for file in glob(os.path.join(path, EXT))]
-
 urls = []
 for file in temp_cfn:
     pdf_file = pikepdf.Pdf.open(file)
@@ -23,7 +14,7 @@ for file in temp_cfn:
         for annots in page.get('/Annots'):
             uri = annots.get('/A').get('/URI')
             if(uri is not None):
-                print(' [+] URL FOUND ', uri)
+                print("\n\n",'[FILE] '+ str(file) +'\n [+] URL FOUND ', uri)
                 urls.append(urls)
 
 print("[*] Total URLs extracted:", len(urls))
