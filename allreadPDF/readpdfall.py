d1aa85b2e208b4b4c97c0a9a46e74964e8948838
Puneeth P <puneeth1996p@gmail.com>
2020-09-03 17:34:24 +0530

    quick commit wiht code

--- allreadPDF/readpdfall.py
+++ allreadPDF/readpdfall.py
@@ -8,7 +8,7 @@ import dateutil.parser
 from datetime import datetime
 import dateparser
 import pandas as pd
-
+import PyPDF2 
 
 PATH = r'C:\Users\punee\Desktop\readLinks_pdf_download\test'
 EXT = "*.pdf"
@@ -18,4 +18,10 @@ temp_cfn = [file
 
 for fle in temp_cfn:
     print(fle)
-
+    # creating a pdf file object 
+    pdfFileObj = open(fle, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    print(pdfReader.numPages)
+    pageObj = pdfReader.getPage(0)
+    print(pageObj.extractText()) 
+    pdfFileObj.close()
